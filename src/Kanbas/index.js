7a0c58ca35a1030b643273bb85d6a732c26453a4
NakedMoleRat8 <charliecosta2003@gmail.com>
2023-11-21 16:19:11 -0500

    Start of assignment 6

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -5,6 +5,10 @@ import Courses from "./Courses";
 import './index.css';
 import store from "./store";
 import {Provider} from "react-redux";
+import SignIn from "./Users/SignIn";
+import Account from "./Users/Account";
+import UserTable from "./Users/UserTable";
+import React from "react";
 
 function Kanbas() {
 
@@ -14,10 +18,10 @@ function Kanbas() {
                 <KanbasNavigation/>
                 <div className="content-wrapper wrapper ms-md-100 ms-sm-15">
                     <Routes>
-                        <Route path="/" element={<Navigate to="Dashboard"/>}/>
-                        <Route path="Account" element={<h1>Account</h1>}/>
+                        <Route path="/" element={<Navigate to="signin"/>}/>
+                        <Route path="Account" element={<Account/>}/>
                         <Route path="Dashboard" element={<Dashboard/>}/>
-                        <Route path="Courses/:courseId/*" element={<Courses />}/>
+                        <Route path="Courses/:courseId/*" element={<Courses/>}/>
                         <Route path="Courses" element={<h1>Courses</h1>}/>
                         <Route path="Calendar" element={<h1>Calendar</h1>}/>
                         <Route path="Inbox" element={<h1>Inbox</h1>}/>
@@ -25,6 +29,8 @@ function Kanbas() {
                         <Route path="Studio" element={<h1>Studio</h1>}/>
                         <Route path="Commons" element={<h1>Commons</h1>}/>
                         <Route path="Help" element={<h1>Help</h1>}/>
+                        <Route path="signin" element={<SignIn/>}/>
+                        <Route path="/admin/users" element={<UserTable/>}/>
                     </Routes>
                 </div>
             </div>
